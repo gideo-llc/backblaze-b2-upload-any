213dcdb0a3ba36146728eb454fe00c852622eaff
Chris Howie <me@chrishowie.com>
2020-02-21 18:39:02 -0500

    Add optional crypto-async dependency

--- hash-buffer.js
+++ hash-buffer.js
@@ -1,7 +1,17 @@
-const crypto = require('crypto');
+try {
+    const asyncHash = require('@ronomon/crypto-async').hash;
+    const { promisify } = require('./promise-utils');
 
-module.exports = buf => {
-    const h = crypto.createHash('sha1');
-    h.update(buf);
-    return h.digest('hex');
-};
+    const promiseHash = promisify(asyncHash);
+
+    module.exports = async buffer =>
+        (await promiseHash('sha1', buffer)).toString('hex');
+} catch (err) {
+    const crypto = require('crypto');
+
+    module.exports = buf => {
+        const h = crypto.createHash('sha1');
+        h.update(buf);
+        return Promise.resolve(h.digest('hex'));
+    };
+}
